d2910c4ecd73bbb893c6dbea130d46cdc73aeec0
GuillermoCuesta <gscuestac@correo.udistrital.edu.co>
2023-12-01 13:38:44 -0500

    Creaciion de Clases bloque especificas

---
+++ Assets/Scripts/BloqueAssignmentClose.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BloqueAssignmentClose : Bloque
+{
+
+    public bool isSelect;
+
+    public string valorXML = "</assignment>";
+
+}
+
